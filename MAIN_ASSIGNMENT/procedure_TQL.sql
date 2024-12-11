f630d9a2a04fde8252b706b59cf5ea620c2d2acd
TaQuangLinh <161272115+TaQuangLinh@users.noreply.github.com>
2024-12-11 13:48:40 +0700

    Add files via upload

---
+++ MAIN_ASSIGNMENT/procedure_TQL.sql
@@ -0,0 +1,30 @@
+DELIMITER //
+-- tìm thông tin người chơi qua quốc tịch: getPlayerByNation 
+CREATE PROCEDURE getPlayerByNation(IN nationName VARCHAR(3))
+BEGIN
+    SELECT *
+    FROM players 
+    WHERE nationality = nationName;
+END //
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS getPlayerByHand;
+DELIMITER //
+-- tìm thông tin người chơi tay thuận: getPlayerByHand 
+CREATE PROCEDURE getPlayerByHand(IN thisHand VARCHAR(1))
+BEGIN
+    SELECT *
+    FROM players 
+    WHERE hand = thisHand;
+END //
+
+DELIMITER ;
+
+
+
+
+
+
+

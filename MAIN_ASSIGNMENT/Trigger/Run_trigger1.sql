5b616db1573c6d4a487b9794dc6ea473bc67e6a6
huydoan2706 <166465466+huydoan2706@users.noreply.github.com>
2024-12-15 23:07:30 +0700

    Add files via upload

---
+++ MAIN_ASSIGNMENT/Trigger/Run_trigger1.sql
@@ -0,0 +1,9 @@
+USE tennis;
+
+INSERT INTO players (player_id, first_name, last_name, hand, date_of_birth, nationality, height)
+VALUES (5, 'Novak', 'Djokovic', 'R', '1987-05-22', 'SRB', 188);
+
+INSERT INTO players (player_id, first_name, last_name, hand, date_of_birth, nationality, height)
+VALUES (6, 'Novak', 'Djokovmic', 'L', '1984-05-22', 'SRB', 188);
+
+SELECT * FROM player_audit;
---
+++ MAIN_ASSIGNMENT/Trigger/Run_trigger2.sql
@@ -0,0 +1,5 @@
+UPDATE single_ranking
+SET ranking = 2, points = 1200
+WHERE player = 5;
+
+SELECT * FROM ranking_history;

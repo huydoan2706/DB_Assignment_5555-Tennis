6d5814ee0ad14c8e3f7ace1cea8ef776a42a295a
huydoan2706 <166465466+huydoan2706@users.noreply.github.com>
2024-12-26 09:34:53 +0700

    Update Run_trigger2.sql

--- MAIN_ASSIGNMENT/Trigger/Run_trigger2.sql
+++ MAIN_ASSIGNMENT/Trigger/Run_trigger2.sql
@@ -1,5 +1,11 @@
-UPDATE single_ranking
-SET ranking = 2, points = 1200
-WHERE player = 1;
+-- Made by Nguyen Duc Huy
+USE tennis;
 
+UPDATE single_ranking
+SET ranking = 25, points = 1000
+WHERE player = 210097;
+
+UPDATE single_ranking
+SET ranking = 20, points = 1000
+WHERE player = 210097;
 SELECT * FROM ranking_history;
